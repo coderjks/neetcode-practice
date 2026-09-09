6ca91a359d3cd85d80eef4f60b6cd01f67217f57
JITENDRA KUMAR SAXENA <jitendrakumar366@gmail.com>
2026-09-09 21:42:48 +0700

    Add: valid-tree - submission-1

---
+++ Data Structures & Algorithms/valid-tree/submission-1.py
@@ -0,0 +1,48 @@
+class DSU:
+
+    def __init__(self, n):
+        self.root = [i for i in range(n)]
+        self.rank = [1 for i in range(n)] # keep the tree balance
+    
+    def find(self, x) -> int:
+        if x == self.root[x]:
+            return x
+        # path compression technique
+        self.root[x] = self.find(self.root[x])
+        return self.root[x]
+    
+    def union(self, x, y) -> bool:
+        rootX = self.find(x)
+        rootY = self.find(y)
+
+        if rootX != rootY:
+            if self.rank[rootX] > self.rank[rootY]:
+                self.root[rootY] = rootX
+            elif self.rank[rootX] < self.rank[rootY]:
+                self.root[rootX] = rootY
+            else:
+                self.root[rootY] = rootX
+                self.rank[rootX] += 1
+            return True
+        
+        return False
+    
+    def isConnected(self, x, y) -> bool:
+        return self.find(x) == self.find(y)
+
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        du = DSU(n)
+        res = n
+
+        for u, v in edges:
+            # These are already connected 
+            if not du.union(u, v):
+                return False
+            res -= 1
+        
+        return True if res == 1 else False
+
+                
+
+        
